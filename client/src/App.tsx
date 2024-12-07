c22095c198f532a5b9033f8de7b80033209e85e4
Johannes Krumm <jkrumm@pm.me>
2024-12-07 11:00:41 +0100

    feat(frontend): integrated Preact Signals

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import { ResortListDto } from "../../shared/dtos/weather.dto.ts";
+import { computed, signal } from "@preact/signals-react";
+import { useSignals } from "@preact/signals-react/runtime";
+
+const resorts = signal<ResortListDto[]>([]);
+
+const favorites = signal<string[]>([
+  "fieberbrunn",
+  "kitzbuehel-kirchberg",
+  "axamer-lizum",
+  "hochfuegen",
+]);
+
+const resortIds = computed<string[]>(() => {
+  return resorts.value.map((resort) => resort.id);
+});
+
+const favoriteResorts = computed<ResortListDto[]>(() => {
+  return resorts.value.filter((resort) => favorites.value.includes(resort.id));
+});
 
 function App() {
-  const [resorts, setResorts] = useState<ResortListDto[]>([]);
+  useSignals();
 
   const fetchResorts = async () => {
-    if (resorts.length > 0) return;
+    if (resorts.value.length > 0) return;
     const response = await fetch("http://localhost:8000/api/resorts");
-    setResorts(await response.json());
+    resorts.value = (await response.json()) as ResortListDto[];
   };
 
   useEffect(() => {
     fetchResorts().then();
-  }, []);
+  }, [favorites]);
 
   return (
     <>
       <h1>Resorts</h1>
       <div>
-        {resorts.map((resort: ResortListDto) => (
-          <div key={resort.id}>{resort.name}</div>
+        {favoriteResorts.value.map((resort: ResortListDto) => (
+          <div key={resort.id}>{resort.id}</div>
         ))}
+      </div>
+      <br />
+      <br />
+      <div>
+        {resortIds.value.map((id) => <div key={id}>{id}</div>)}
       </div>
     </>
   );
