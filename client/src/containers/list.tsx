bc9341eea0ebb10cac38d0ce3671826ea37713a3
jkrumm <jkrumm@pm.me>
2024-12-08 09:58:46 +0100

    feat(frontend): added Favicon and title

---
+++ client/src/containers/list.tsx
@@ -0,0 +1,12 @@
+export function List() {
+  return (
+    <div>
+      <h1>Resorts</h1>
+      <ul>
+        <li>Resort 1</li>
+        <li>Resort 2</li>
+        <li>Resort 3</li>
+      </ul>
+    </div>
+  );
+}
